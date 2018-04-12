fb39f8c3b26032fb01a719f31195330f58f35a48
Brent Bauman <brentbauman86@gmail.com>
2018-04-12 21:14:29 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,8 +1,8 @@
-
-def countdown(10)
-while (10) > 0
-  puts "#{10} SECOND(S)!"
-  (10) -= 0
+x = 10
+def countdown(x)
+while x > 0
+  puts "#{x} SECOND(S)!"
+  x -= 0
 end
 puts "HAPPY NEW YEAR!"
 end
